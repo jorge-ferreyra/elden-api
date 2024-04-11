92a7a8c71efbca03e77a58a93b93f5bef05f67ae
Jorge Ferreyra <jorxfer@gmail.com>
2024-04-11 18:24:53 -0300

    Updating project

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const fs = require('node:fs')
 const app = express()
 const PORT = 1234
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('<h1>Para encontrar el JSON con la información, ir a "/bosses"...</h1>')
@@ -9,6 +12,22 @@ app.get('/', (req, res) => {
 
 /// ENDPOINT
 app.get('/bosses', (req, res) => {
+  fs.readFile('./data.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send('Error al leer el archivo')
+    }
+    try {
+      const info = JSON.parse(data)
+      res.json(info)
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('Error al procesar los datos')
+    }
+  })
+})
+
+app.get('/bosses/:id', (req, res) => {
   fs.readFile('./data.json', 'utf-8', (err, data) => {
     if (err) {
       console.error(err)
